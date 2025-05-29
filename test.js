68a343ac2f6ac20dc6b3e3721666926be57ffca3
Jordan <jordan@smartdust.me>
2025-05-29 18:14:25 +0200

    Added example Appium Android Chrome browser automation config for SmartDust devices

--- test.js
+++ test.js
@@ -1,25 +1,29 @@
 const wdio = require("webdriverio");
 
+// run Appium server with parameters: `appium --allow-insecure chromedriver_autodownload`
+
 const sharedCapabilities = {
     'appium:automationName': 'UiAutomator2',
     'platformName': 'Android',
     'appium:uiautomator2ServerInstallTimeout': 60000,
-    'appium:adbExecTimeout': 30000
+    'appium:adbExecTimeout': 30000,
+    'browserName': 'chrome',
+    'appium:chromedriverExecutableDir': 'chromedrivers', // https://github.com/appium/appium-uiautomator2-driver/tree/master?tab=readme-ov-file#automatic-discovery-of-compatible-chromedriver
+    'wdio:enforceWebDriverClassic': true // https://webdriver.io/blog/2024/08/15/webdriverio-v9-release/#new-features
 }
 const devicesCapabilities = {
-    'appium:udid': 'public.smartdust.me:12891'
+  'appium:udid': 'demo.smartdust.me:13090'
 }
 const opts = {
-    path: '/',
-    port: 4723,
-    //maxInstances: 1,
+  path: '/',
+  port: 4723,
 };
 
 async function main() {
   opts.capabilities = {...sharedCapabilities, ...devicesCapabilities}
   client = await wdio.remote(opts)
-  await client.activateApp("com.google.android.googlequicksearchbox")
-  await client.deleteSession()
+  await client.navigateTo("https://smartdust.me") // https://webdriver.io/docs/api/webdriver#navigateto
+  await client.getUrl();
 }
 
 main();
